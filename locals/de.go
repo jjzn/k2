38bf8d4dda6b625d1eb36136b3712b87829cc869
jjzn <50965831+jjzn@user.noreply.github.com>
2022-12-10 23:33:29 +0100

    Add translations for German language (not tested)
    There have not been done any tests to this translation. Don't merge this
    to main and deploy until the necessary tests have been done and the
    translations' quality is ensured.

---
+++ locals/de.go
@@ -0,0 +1,72 @@
+package main
+
+var localStrings = map[string]string{
+	"localeName": "de",
+
+	"removeItemConfirm": "Bist du sicher, dass du diesen Eintrag löschen möchtest?",
+	"removeItem": "Eintrag löschen",
+	"editItem": "Eintrag bearbeiten",
+	"invite": "Personen einladen",
+
+	"gridViewTitle": "Kalenderansicht",
+	"listViewTitle": "Listenansicht",
+
+	"inviteViewTitle": "Einladung zu",
+	"inviteMessage": "Du wurdest zu",
+	"invitePersonsJoiner": "eingeladen, mit",
+	"inviteInstructions": "Wenn du teilnehmen möchtest, füge deinen Namen hinzu, und der Eintrag wird automatisch geändert.",
+
+	"labelTitle": "Titel",
+	"labelPersons": "Teilnehmende (durch Kommata getrennt)",
+	"labelDate": "Datum",
+	"labelTime": "Uhrzeit (freiwillig)",
+	"labelEndDate": "Enddatum (freiwillig)",
+	"labelEndTime": "Enduhrzeit (freiwillig)",
+	"labelAddDescription": "Beschreibung hinzufügen",
+	"labelDescription": "Beschreibung (freiwillig)",
+	"labelName": "Name",
+	"labelCreate": "Erstellen",
+	"labelJoin": "Teilnehmen",
+	"labelNew": "Neu",
+	"labelUpdate": "Ändern",
+
+	"dateToday": "Heute",
+	"dateTomorrow": "Morgen",
+	"dateThisWeek": "Diese Woche",
+	"dateNextWeek": "Nächste Woche",
+	"dateThisMonth": "Dieser Monat",
+	"dateNextMonth": "Nächster Monat",
+
+	"dayMon": "Mo",
+	"dayTue": "Di",
+	"dayWed": "Mw",
+	"dayThu": "Do",
+	"dayFri": "Fr",
+	"daySat": "Sa",
+	"daySun": "So",
+
+	"messageNoItems": "Keine Einträge",
+	"wordItem": "Eintrag",
+}
+
+var monthNames = []string{
+	"Januar", "Februar", "März", "April", "May", "Juni",
+	"Juli", "August", "September", "October", "November", "Dezember",
+}
+
+var dayNames = []string{
+	"Montag", "Dienstag", "Mitwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag",
+}
+
+var localFormats = map[string]string{
+	"daysLeft": " (noch %d Tage)",
+	"daysPast": " (vor %d Tagen)",
+	"today": " (heute)",
+	"untilDate": " — bis zum ",
+	"untilTime": " — bis ",
+	"at": " um ",
+}
+
+func getLocalString(key string) string {
+	return localStrings[key]
+}
