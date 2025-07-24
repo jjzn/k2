797eb15e3c3c617037f518e719f919c4cc7a5fc6
jjzn <50965831+jjzn@users.noreply.github.com>
2025-07-24 22:34:17 +0200

    Improve location suggestion

--- static/suggest-location.js
+++ static/suggest-location.js
@@ -1,16 +1,26 @@
 const form = document.forms[0];
 const location_field = form['location'];
+const list = document.querySelector('#location-suggestions');
 
 const MAX_SUGGESTIONS = 3;
+const TYPING_THRESHOLD_MS = 500;
 
-// TODO: provide attribution
-// TODO: determine wether to listen to input or blur events
-location_field.addEventListener('input', async () => {
+let typing_timeout;
+
+// TODO: update version
+const headers = new Headers({
+    'User-Agent': 'k2/0.2'
+});
+
+const attribution = document.createElement('p');
+attribution.innerHTML = 'Data from <a href="https://openstreetmap.org/copyright">OpenStreetMap</a>';
+
+async function load_suggestions() {
     const query = encodeURIComponent(location_field.value);
-    const resp = await fetch(`https://nominatim.openstreetmap.org/search?q=${query}&limit=${MAX_SUGGESTIONS}&format=jsonv2`); // TODO: set the user agent
+    // TODO: cache results in the server
+    const resp = await fetch(`https://nominatim.openstreetmap.org/search?q=${query}&limit=${MAX_SUGGESTIONS}&format=jsonv2`, { headers });
     const data = await resp.json();
 
-    const list = document.querySelector('#location-suggestions');
     list.innerHTML = ''; // clear the list
 
     // TODO: constuct custom name based on the address fields
@@ -22,12 +32,23 @@ location_field.addEventListener('input', async () => {
         link.href = 'javascript:';
         link.addEventListener('click', () => {
             location_field.value = place.display_name;
+            list.innerHTML = ''; // clear the suggestions list on click
         });
 
         item.append(link);
         list.append(item);
+        list.append(attribution);
     }
 
     list.hidden = false;
     location_field.after(list);
+}
+
+location_field.addEventListener('input', async () => {
+    // Prevent location suggestions while still typing, we don't want to
+    // overload the server with requests
+    clearTimeout(typing_timeout);
+    typing_timeout = setTimeout(() => load_suggestions(), TYPING_THRESHOLD_MS);
+
+    list.innerHTML = ''; // clear the list
 });
